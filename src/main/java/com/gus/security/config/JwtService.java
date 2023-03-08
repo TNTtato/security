c344d0a528713818f10fa3ed78b6438bdb74335d
Gustavo <penarandagustavo5@gmail.com>
2023-03-08 17:22:12 -0500

    implemet a method to extract all claims from a token

--- src/main/java/com/gus/security/config/JwtService.java
+++ src/main/java/com/gus/security/config/JwtService.java
@@ -1,10 +1,32 @@
 package com.gus.security.config;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
 
 @Service
 public class JwtService {
+
+    private static final String SECRET_KEY = "4D6251655468576D5A7133743677397A24432646294A404E635266556A586E32";
     public String extractUsername(String token) {
         return null;
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
