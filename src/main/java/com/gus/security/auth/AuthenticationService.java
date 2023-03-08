2ce935e87230ce473e650f86c83415ab303360d8
Gustavo <penarandagustavo5@gmail.com>
2023-03-08 18:51:46 -0500

    implement register service method

--- src/main/java/com/gus/security/auth/AuthenticationService.java
+++ src/main/java/com/gus/security/auth/AuthenticationService.java
@@ -1,13 +1,34 @@
 package com.gus.security.auth;
 
+import com.gus.security.config.JwtService;
+import com.gus.security.user.Role;
+import com.gus.security.user.User;
+import com.gus.security.user.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class AuthenticationService {
+
+    private final UserRepository repository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+
     public AuthenticationResponse register(RegisterRequest request) {
-        return null;
+        var user = User.builder()
+                .firstname(request.getFirstname())
+                .lastname(request.getLastname())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .build();
+        repository.save(user);
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
     }
 
     public AuthenticationResponse authenticate(AuthenticationRequest request) {
